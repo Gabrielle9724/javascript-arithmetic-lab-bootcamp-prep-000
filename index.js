cc8a59e87568b119893c49b4359a5b71a3047e78
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-07 22:02:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ function add(){}
 function subtract(){}
 function multiply(){}
 function divide(){}
-function add(a, b){
-  (a + b)
+
+var number = 10
+
+function add5() {
+  number += 5
 }
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number) // 3.333333333335
+
+add5()
+
+console.log(number) // 8.333333333335
+
+// reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
