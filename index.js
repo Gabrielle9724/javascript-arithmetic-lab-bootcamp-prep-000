fd8b2715bd39278d1b1e31d07121c3df9008a786
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-07 22:06:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function subtract(a, b){
   number= a - b
   return number
 }
+
+function multiply(a, b){
+  number= a * b
+  return number
+}
+
+function divide(a, b){
+  number= a / b
+  return number
+}
